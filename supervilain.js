84debc8a1f1999099618beb423de15a7814fbc97
YvertHugo <hugo.yvert@gmail.com>
2024-02-13 22:14:16 +0100

    modif front vilains

--- supervilain.js
+++ supervilain.js
@@ -4,18 +4,3 @@ const sidebar = document. querySelector ('.sidebar');
         toggleBtn.addEventListener('click', () => {
             sidebar.classList. toggle('active');
         }) ;
-
-
-
-
-        document.addEventListener('DOMContentLoaded', function() {
-            var angle = 0;
-            document.getElementById('prev').addEventListener('click', function() {
-              angle += 30; // Ajustez ce nombre en fonction du nombre de faces et de l'angle que vous avez défini
-              document.querySelector('.carousel').style.transform = 'rotateY(' + angle + 'deg)';
-            });
-            document.getElementById('next').addEventListener('click', function() {
-              angle -= 30; // Ajustez ce nombre en fonction du nombre de faces et de l'angle que vous avez défini
-              document.querySelector('.carousel').style.transform = 'rotateY(' + angle + 'deg)';
-            });
-          });
