c7861ace5bed811cee7654cc0877f19199085d00
Clém <clemauger01@orange.fr>
2024-02-14 16:59:38 +0100

    ok

---
+++ Avengers/src/main/java/com/java/Avengers/AvengersApplication.java
@@ -0,0 +1,13 @@
+package com.java.Avengers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvengersApplication.class, args);
+	}
+
+}
