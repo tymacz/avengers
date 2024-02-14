45fb72fa49ed2abfad150edc3fbfefe2ad1765cc
YvertHugo <hugo.yvert@gmail.com>
2024-02-14 16:51:03 +0100

    Modif front vilain

--- supervilain.js
+++ supervilain.js
@@ -4,3 +4,32 @@ const sidebar = document. querySelector ('.sidebar');
         toggleBtn.addEventListener('click', () => {
             sidebar.classList. toggle('active');
         }) ;
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const slideButtons = document.querySelectorAll('.slide-btn');
+    let currentIndex = 0; 
+
+    slideButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const container = document.querySelector('.wrapper');
+            const cardWidth = document.querySelector('.card').clientWidth;
+            const gap = 160; 
+            const moveX = cardWidth + gap;
+
+            if(this.classList.contains('right')) {
+                currentIndex += 12;
+            } else {
+                currentIndex -= 12; 
+            }
+
+            
+            currentIndex = Math.min(Math.max(currentIndex, 0), 24); 
+
+            const shift = -moveX * currentIndex / 12;
+            container.style.transform = `translateX(${shift}px)`;
+        });
+    });
+});
