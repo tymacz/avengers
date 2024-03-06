996f4eea0db2df4bb235c2a6729e30fe45ca0d48
YvertHugo <hugo.yvert@gmail.com>
2024-03-06 16:04:17 +0100

    pas finit

--- mission.js
+++ mission.js
@@ -1,25 +1,30 @@
-fetch("https://65e59930d7f0758a76e6cc47.mockapi.io/adresse")
+fetch("http://10.191.14.114:8080/Mission")
     .then(response => response.json())
     .then(data => {
         data.forEach((user, index) => {
             h2 = document.createElement("h2")
-            h2.textContent = "NOM DE MISSION : " + user.nom
-            content = document.getElementsByClassName("content")
-            content[(user.id)-1].appendChild(h2)
+            h2.textContent = "NOM DE MISSION : " + user.titre
+            content = document.getElementsByClassName("content-left")
+            content[(user.id_mission)-1].appendChild(h2)
 
             h2 = document.createElement("h2")
-            h2.textContent = "TYPE : " + user.type
-            content = document.getElementsByClassName("content")
-            content[(user.id)-1].appendChild(h2)
+            h2.textContent = "TYPE : " + user.nature
+            content = document.getElementsByClassName("content-left")
+            content[(user.id_mission)-1].appendChild(h2)
 
             h2 = document.createElement("h2")
-            h2.textContent = "LOCALISATION : " + user.localistion
-            content = document.getElementsByClassName("content")
-            content[(user.id)-1].appendChild(h2)
+            h2.textContent = "INFO : " + user.info
+            content = document.getElementsByClassName("content-left")
+            content[(user.id_mission)-1].appendChild(h2)
 
             h2 = document.createElement("h2")
-            h2.textContent = "MÉCHANT : " + user.mechant
-            content = document.getElementsByClassName("content")
-            content[(user.id)-1].appendChild(h2)
+            h2.textContent = "NIVEAU DE GRAVITÉ : " + user.niveau_gravite
+            content = document.getElementsByClassName("content-left")
+            content[(user.id_mission)-1].appendChild(h2)
+
+            h2 = document.createElement("h2")
+            h2.textContent = "NIVEAU DE GRAVITÉ : " + user.niveau_urgence
+            content = document.getElementsByClassName("content-left")
+            content[(user.id_mission)-1].appendChild(h2)
     })
 })
