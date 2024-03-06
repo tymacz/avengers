0792f3aea3df322f383bed7b2d81878cfca66109
tymacz <75813559+tymacz@users.noreply.github.com>
2024-03-06 10:37:51 +0100

    Revert "Ajout des features page super hero pour recevoir les datas de l'API"

--- supervilain.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document. querySelector ('.sidebar');
-        const toggleBtn = document. querySelector('.toggle-btn');
-        
-        toggleBtn.addEventListener('click', () => {
-            sidebar.classList. toggle('active');
-        }) ;
