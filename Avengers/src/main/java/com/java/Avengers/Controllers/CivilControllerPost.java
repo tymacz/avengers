23b30d100a4433e2b062be9a8f99ff1f4406e733
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-03-14 09:08:26 +0100

    sauvegarde de code

--- Avengers/src/main/java/com/java/Avengers/Controllers/CivilControllerPost.java
+++ Avengers/src/main/java/com/java/Avengers/Controllers/CivilControllerPost.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/civils")
@@ -17,13 +18,33 @@ public class CivilControllerPost {
     private CivilServicePost civilServicePost;
 
     @PostMapping
+
     public CivilEntityPost addCivil(
             @RequestParam String nom_civil,
-            @RequestParam String prenom_civil
+            @RequestParam String prenom_civil,
+            @RequestParam String genre,
+            @RequestParam Date date_naissance,
+            @RequestParam String nationalite,
+            @RequestParam String organisation,
+            @RequestParam String commentaire,
+            @RequestParam Integer nb_incident,
+            @RequestParam Integer nb_mission,
+            @RequestParam Integer id_adresse,
+            @RequestParam String numero_tel
     ) {
         CivilEntityPost newCivil = new CivilEntityPost();
         newCivil.setNom_civil(nom_civil);
         newCivil.setPrenom_civil(prenom_civil);
+        newCivil.setGenre(genre);
+        newCivil.setDate_naissance(date_naissance);
+        newCivil.setNationalite(nationalite);
+        newCivil.setOrganisation(organisation);
+        newCivil.setCommentaire(commentaire);
+        newCivil.setNb_incident(nb_incident);
+        newCivil.setNb_mission(nb_mission);
+        newCivil.setId_adresse(id_adresse);
+        newCivil.setNumero_tel(numero_tel);
+
         return civilServicePost.addCivil(newCivil);
     }
 
--- Avengers/src/main/java/com/java/Avengers/Entities/CivilEntityPost.java
+++ Avengers/src/main/java/com/java/Avengers/Entities/CivilEntityPost.java
@@ -1,19 +1,35 @@
 package com.java.Avengers.Entities;
-
+import java.util.Date;
 
 import jakarta.persistence.*;
 import lombok.Setter;
 
 @Setter
-
 @Entity
 @Table(name = "civil")
-
 public class CivilEntityPost {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_civil;
 
     private String nom_civil;
     private String prenom_civil;
+    private Date date_naissance;
+    private String genre;
+    private Date date_deces;
+    private String nationalite;
+    private String organisation;
+    private Date date_ajout;
+    private Integer id_adresse;
+    private String numero_tel;
+
+    public void setNb_incident(Integer nbIncident) {
+    }
+
+    public void setNb_mission(Integer nbMission) {
+    }
+
+    public void setCommentaire(String commentaire) {
+    }
 }
